da2c4fa5ed96a99359b5ff4c99adcc70f2c1ab8a
yeroc8 <yeroc8.git@gmail.com>
2019-08-04 13:10:37 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+from asyncio import sleep
+import discord as dis
+from discord.ext import commands as cmd
+import logging
+from jishaku.help_command import DefaultPaginatorHelp
+from uwu import uwu_word
+from secret import token
+
+logging.basicConfig(level=logging.INFO)
+bot = cmd.Bot(command_prefix=';', help_command=DefaultPaginatorHelp(),
+              activity=dis.Activity(name=";help", type=dis.ActivityType.watching))
+bot.load_extension('jishaku')
+
+
+@bot.command()
+async def uwu(ctx, *, text):
+    """Change any text to UwU-speak"""
+    text = text.replace("fuck", "fwick").replace("shit", "poopoo").replace("bitch", "meanie").replace(
+        "ass", "butt").replace("father", "daddy")
+    await ctx.send(' '.join([uwu_word(word) for word in text.lower().split()]))
+
+
+@bot.command()
+async def kick(ctx, member: dis.Member, *, reason="(no reason given)"):
+    """Calls a vote on whether to kick a member"""
+    message = await ctx.send('''*{} wants to call a vote:*
+**Kick member: {}?**
+**{}**
+Press ✅ to vote YES
+Press ❌ to vote NO
+*Vote Count:*'''.format(ctx.author.display_name, member.display_name, reason))
+    await message.add_reaction('✅')
+    await message.add_reaction('❌')
+    await sleep(3)  # 170
+    await ctx.trigger_typing()
+    await sleep(10)
+    message = dis.utils.get(ctx.channel, id=message.id)
+    if message.reactions[0].count > message.reactions[1].count:
+        await ctx.send("✅ Vote Passed\nKicking member: " + member.display_name)
+        # lock user to hidden channel temporarily?
+    else:
+        await ctx.send("❌ Vote Failed")
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, cmd.CommandNotFound):
+        await ctx.send("Error: Command not found")
+
+bot.run(token)
---
+++ uwu.py
@@ -0,0 +1,40 @@
+import random
+
+joy = [" (\\* ^ ω ^)", " (o^▽^o)", " (≧◡≦)", " ☆⌒ヽ(\\*\"､^\\*)chu", " ( ˘⌣˘)♡(˘⌣˘ )", " xD"]
+embarrassed = [" (⁄ ⁄>⁄ ▽ ⁄<⁄ ⁄)..", " (\\*^.^\\*)..,", "..,", ",,,", "... ", ".. ", " mmm..", "O.o"]
+confused = [" (o_O)?", " (°ロ°) !?", " (ーー;)?", " owo?"]
+sparkles = [" \\*:･ﾟ✧\\*:･ﾟ✧ ", " ☆\\*:・ﾟ ", "〜☆ ", " uguu.., ", "-.-"]
+
+
+def uwu_word(word):
+    end = ''
+    # randomly change punctuation to kaomoji
+    if word[-1] in ',.?!':
+        end = word[-1]
+        word = word[:-1]
+        if end in ',.':
+            chance = 0.33
+        else:
+            chance = 0.50
+        if random.random() < chance:
+            if end == ',':
+                end = random.choice(embarrassed)
+            elif end == '?':
+                end = random.choice(confused)
+            else:
+                end = random.choice(joy)
+        if random.random() < 0.25:
+            end = random.choice(sparkles)
+    # don't change some endings
+    if word.endswith(('le', 'll', 'er', 're')):
+        end = word[-2:] + end
+        word = word[:-2]
+    elif word.endswith(('les', 'lls', 'ers', 'res')):
+        end = word[-3:] + end
+        word = word[:-3]
+    # uwu
+    word = word.replace('l', 'w').replace('r', 'w') + end
+    # random stutter
+    if len(word) > 2 and word[0].isalpha() and random.random() < 0.17:
+        word = word[0] + '-' + word
+    return word
